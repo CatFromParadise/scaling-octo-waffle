ccf94fac21cffc3c8d1ab7f7eac9caa3ff9280f4
CatFromParadise <73900921+CatFromParadise@users.noreply.github.com>
2020-11-03 19:43:28 +0300

    Add files via upload

---
+++ Source3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+void main()
+{
+	double x, y;
+	cout << "input x, y" << endl;
+	cin >> x >> y;
+	double r1, r2;
+	r1 = 1 + x / 2 + y / 4;
+	r2 = pow((abs(x - 1) - pow(y, 1 / 3)), 1 / 2);
+	cout <<"example is equal to: " << r2 / r1;
+}
